d538efebf37c36b80508d34287ed0f5176dc58c4
chandantm317 <chandantmchandu31@gmail.com>
2024-02-06 11:38:32 +0530

    type conversion added

---
+++ 6type conversion/index.js
@@ -0,0 +1,27 @@
+// process of convverting data of one type to another type
+//2 types
+// implicit == automatic type c
+// explicit == manula t c
+
+// implicit
+// ex
+// number + string = string
+let result = "3" + 2;
+console.log(result);
+console.log(typeof result);
+
+// numeric string used with + gives string type
+
+// numeric string used with - , / , * results number type
+// JavaScript Explicit Conversion
+
+let number;
+// string to number
+number = Number("123");
+console.log(number);
+console.log(typeof number);
+
+let string;
+string = String(122);
+console.log(string);
+console.log(typeof string);

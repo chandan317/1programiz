516ae118b60511f90b6484e7272e932f8c41aefc
chandantm317 <chandantmchandu31@gmail.com>
2024-02-06 10:58:10 +0530

    comments added

--- 5Comments/index.js
+++ 5Comments/index.js
@@ -1,1 +1,35 @@
 // comments
+
+// hints programmer can add to make their code easy to read and understand
+// single line comment
+/* multiple 
+line
+ comments 
+ multiple
+*/
+// starts with is a single line comment
+naam = "chandan";
+
+// printing the name on the  console
+console.log("hello     " + naam);
+
+movie = "animal";
+
+// comment
+console.log(movie); //printing the movie on the console
+
+// multiple comment
+/* rules or any multiple line thing we can use this*/
+
+// comments for debugging
+
+// comnt can be used to disable code to prevent it from being executed
+// ctrl + /
+/* shift + alt +  A*/
+
+console.log("some code");
+// console.lofg("Error  code");
+console.log("other code");
+//  /* As a general rule of thumb, use comments to explain why you did something rather than how you did something, and you are good. */
+
+/* Note: Comments shouldn't be the substitute for a way to explain poorly written code in English. You should always write well-structured and self-explanatory code. And, then use comments. */

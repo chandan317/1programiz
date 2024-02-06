2ee9e2c64f61f905016306e79689083af58d457f
chandantm317 <chandantmchandu31@gmail.com>
2024-02-06 09:41:53 +0530

    operators added

---
+++ 4Operators/index.js
@@ -0,0 +1,89 @@
+//js operators
+
+//operator
+
+// operator is a special symbol that used to perform operations on operands
+2 + 3; // 6
+
+// here 2,3 are operands + operator
+
+4 + 5; //4,5 are operands  + is the operator
+
+// js operator types
+
+// assignment operators
+// arithmetic operators
+// Comparision operators
+// logical operators
+// bitwise operators
+// string operators
+// other operators
+
+//assignment operators
+
+const x = 5;
+// = is operator
+
+// assignment operators are used to assign values to the variable
+
+const y = 3;
+
+// operatos common
+
+// = assignment operator
+const a = 7;
+
+// += additional assignment
+// let b = 5;
+// b += 6;
+// console.log(b);
+// -= subtraction assignment
+// c -= 6;
+
+// *= multiplication asssgnment
+// d *= 7;
+
+// /= division assignment
+// e /= 8;
+// %= remainder assignment
+// f %= 9;
+// **= exponentiation assignment
+// a **= 70; // a = a **70
+
+// js arithmetic operators
+
+// used to perform arithmetic calculations
+
+// const number = 3 + 5;
+// console.log(number);
+
+//+ - * % / ++ ** --
+
+// comaprision operator
+const ab = 3,
+  bc = 2;
+console.log(ab > bc);
+//compare two values and returns a boolean value true or false
+
+// ==  !=  === !==  >  >=  <  <=
+
+// js logical operators
+
+//performs logical operations return boolean value
+
+// && || !
+
+// js bitwise operator
+// binary rarely used
+//& | ^ ~ << >> >>>
+
+// js sr=tring operator
+// + oprator to concatenate two more strings
+
+//  conacatenation operator
+console.log("hello" + "world");
+
+let o = "javascript";
+o += "tutorial";
+// o= o  + " tutorial"
+console.log(o);

9dd70657f3aabb69da5e1c3e87c1254f996f6a49
chandantm317 <chandantmchandu31@gmail.com>
2024-02-07 12:45:07 +0530

    comparision oprtr added

---
+++ 7Comparisionlogical/index.js
@@ -0,0 +1,55 @@
+//comparision operator
+
+//compare two values and give back a boolean value
+// true or false
+
+// == equal to  5== 5 true
+//  != not equal to 5!= 5 false
+// === strict equal to 5==="5" false
+// == !== dtrict not equal to 5!=="5"
+// greater than >  left is greater 3>2
+// >= greater than or equal to
+// < lesss than
+
+// <= less than or equal to
+
+// == != === !== > >= < <=
+
+// equal operator
+
+const a = 5,
+  b = 2,
+  c = "hello";
+
+// equal to operator
+
+// console.log(a == 5);
+// console.log(b == "2");
+// console.log(c == "Hello");
+
+// not equal to operator
+
+const o = 3,
+  p = "hello";
+//not equal operator
+// console.log(o != 2);
+// console.log(p != "Hello");
+
+// strict equal to operator
+
+const q = 2;
+
+// strict equl operator
+// console.log(q === 2);
+// console.log(q === "2");
+// == evaluates to true if the operands are equal, however, === evaluates to true only if the operands are equal and of the same type
+
+// strict not equal to
+
+const m = 2,
+  n = "hello";
+// strict not equal
+
+console.log(m !== "2");
+console.log(m !== 2);
+console.log(n !== "hello");

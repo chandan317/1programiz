50314f6dc108168a340353ab801f212de901d344
chandantm317 <chandantmchandu31@gmail.com>
2024-02-08 16:55:13 +0530

    if statement added

---
+++ 8if..else/if/index.js
@@ -0,0 +1,51 @@
+// js if else statement
+
+// cprming situaton to run block of code among more than alternatives
+
+// assigning grades a , b, c based on marks obtained by student
+// we use if ... else statement
+// to create program to make decisions
+//if statement
+// if...else
+// if..elseif..else
+
+// js if statement
+
+// syntax of if
+
+// if (condition) {
+// the body of if
+// }
+// if (condition) {
+// body of if
+// }
+
+// condition is true
+// let number = 2;
+// if (number > 0) {
+// code is executed
+// }
+
+// code after if
+// =============================
+// condition false
+
+// let c = -2;
+// if (c > 0) {
+// code not
+// }
+// executes after block
+// code after if
+
+// ============================
+
+// example
+// check the number is positive
+const num = prompt("enter the number:  ");
+// check if number is grater than 0
+
+if (num > 0) {
+  // the body of if satement
+  console.log("the number is positive");
+}
+console.log("the number is easy");

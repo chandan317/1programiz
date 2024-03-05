0f0cdf01cf0855db0d3746006fc733194c7e7263
chandantm317 <chandantmchandu31@gmail.com>
2024-03-05 10:26:50 +0530

    if else.. added

---
+++ 8if..else/if elseee/index.js
@@ -0,0 +1,22 @@
+// if else
+
+// if (condition) {
+// block of code if cond is true
+// }
+// else{
+// if cond is false
+// }
+
+// check if the number is positive or negative/zero
+
+const numb = prompt("Enter a number :");
+//check if number greater than 0
+if (numb > 0) {
+  console.log("the number is positive");
+}
+// if number is not greater than 0
+else {
+  console.log("the numbber is  either a negative number or zero");
+}
+console.log("the if... else stament is easy");
+console.log(" the if.. else statement is easy");
